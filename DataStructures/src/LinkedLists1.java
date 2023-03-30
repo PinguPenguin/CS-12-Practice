58b35adeb8a0323a6d322d438717f26186278472
PinguPenguin <jub.jub.madej@gmail.com>
2023-03-30 11:28:22 -0400

    as

---
+++ DataStructures/src/LinkedLists1.java
@@ -0,0 +1,13 @@
+public class LinkedLists1 {
+    private Node head;
+
+    class Node {
+        int info;
+        Node link;
+        Node (int i, Node n){
+            info = i;
+            link = n;
+        }
+    }
+
+}
