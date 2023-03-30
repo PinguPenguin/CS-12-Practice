811093860896ec56fb8c5c11e3e41e9556d1f647
PinguPenguin <jub.jub.madej@gmail.com>
2023-03-30 12:11:24 -0400

    as

--- DataStructures/src/LinkedLists1.java
+++ DataStructures/src/LinkedLists1.java
@@ -1,6 +1,45 @@
 public class LinkedLists1 {
     private Node head;
 
+    public void deleteFirst(){
+        if (head == null) {
+            System.out.println("Sorry, Dave. I'm afraid I can't do that.");
+        } else {
+            head = head.link.link;
+        }
+    }
+
+    public void deleteLast(){
+        if (head == null) {
+            System.out.println("Sorry, Dave. I'm afraid I can't do that.");
+        } else {
+            head = head.link.link;
+        }
+    }
+
+    public int sum() {
+        int sum = 0;
+        for (Node temp = head; temp!= null; temp = temp.link){
+            sum += temp.info;
+        }
+        return sum;
+    }
+
+    public void addAtFront (int item){
+        head = new Node(item, head);
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty.");
+        } else {
+            for (Node temp = head; temp!=null; temp = temp.link) {
+                System.out.print(temp.info + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
     class Node {
         int info;
         Node link;
--- DataStructures/src/ListTester.java
+++ DataStructures/src/ListTester.java
@@ -1,3 +1,15 @@
 public class ListTester {
-    
+    static LinkedLists1 list1 = new LinkedLists1();
+    public static void main(String[] args) {
+        list1.printList();
+        list1.addAtFront(10);
+        list1.printList();
+        list1.addAtFront(15);
+        list1.printList();
+        list1.deleteFirst();
+        list1.printList();
+        list1.addAtFront(20);
+        list1.printList();
+        //System.out.println("Sum: " + list1.sum());
+    }
 }
