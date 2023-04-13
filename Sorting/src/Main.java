68784e23dc280e7018af0ba7865a44f8fc63fcbc
PinguPenguin <jub.jub.madej@gmail.com>
2023-04-13 12:04:51 -0400

    stacs

---
+++ Sorting/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        ArrayGen gen = new ArrayGen();
+        //naming is hard
+        int[] tenOne = ArrayGen.randomizedArray(10);
+        int[] tenTwo = ArrayGen.randomizedArray(100);
+        int[] tenThree = ArrayGen.randomizedArray(1000);
+        int[] tenFour = ArrayGen.randomizedArray(10000);
+        int[] tenFive = ArrayGen.randomizedArray(100000);
+
+
+    }
+}
