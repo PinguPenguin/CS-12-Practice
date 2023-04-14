f36f04f63303c8a1ce09aeeb2398a1f3de03b04a
PinguPenguin <jub.jub.madej@gmail.com>
2023-04-14 12:07:02 -0400

    asdasdasdasdasdas

--- Sorting/src/Main.java
+++ Sorting/src/Main.java
@@ -2,12 +2,22 @@ public class Main {
     public static void main(String[] args) {
         ArrayGen gen = new ArrayGen();
         //naming is hard
-        int[] tenOne = ArrayGen.randomizedArray(10);
-        int[] tenTwo = ArrayGen.randomizedArray(100);
-        int[] tenThree = ArrayGen.randomizedArray(1000);
-        int[] tenFour = ArrayGen.randomizedArray(10000);
-        int[] tenFive = ArrayGen.randomizedArray(100000);
+        int[] ArrayOne = ArrayGen.randomizedArray(10);
+        int[] ArrayTwo = ArrayGen.randomizedArray(100);
+        int[] ArrayThree = ArrayGen.randomizedArray(1000);
+        int[] ArrayFour = ArrayGen.randomizedArray(10000);
+        int[] ArrayFive = ArrayGen.randomizedArray(100000);
+        for (int i = 0; i < 11; i++){
+            System.out.println(bubbleTime(ArrayThree));
+            ArrayThree = ArrayGen.randomizedArray(1000);
+        }
+    }
 
-
+    public static long bubbleTime(int[] arr){
+        StopWatch watch = new StopWatch();
+        watch.start();
+        Sorts.bubbleSort(arr);
+        watch.stop();
+        return watch.getElapsedTime();
     }
 }
